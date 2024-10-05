dcf2bd0785ff6cc90a275cbf7f4a3f28d8e55633
Samira <kh.mohamady@gmail.com>
2024-10-05 15:23:42 -0600

    image

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,9 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='post_images/', blank=True, null=True) 
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)  # This line adds the image field
+ 
 
 
     def __str__(self):
